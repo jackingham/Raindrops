e686be8fa3ce076a61ae84bec8927b48c1882e81
jackingham <jackingham1998@gmail.com>
2021-05-26 17:21:35 +0100

    Added basic program functionality.

---
+++ src/main/java/com/sparta/jack/Raindrops.java
@@ -0,0 +1,22 @@
+package com.sparta.jack;
+
+public class Raindrops {
+
+    public String raindrops(int n){
+        StringBuilder builder = new StringBuilder();
+        if (n % 3 == 0){
+            builder.append("Pling");
+        }
+        if (n % 5 == 0){
+            builder.append("Plang");
+        }
+        if (n % 7 == 0){
+            builder.append("Plong");
+        }
+        if (builder.length() == 0){
+            return Integer.toString(n);
+        } else {
+            return builder.toString();
+        }
+    }
+}
